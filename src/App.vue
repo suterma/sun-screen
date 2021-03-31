04abada99dbb9d9cd5c5168ad609c8e181f10871
suterma <github@marcelsuter.ch>
2021-03-31 11:51:40 +0200

    added css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="page-container">
-        <md-app md-waterfall md-mode="fixed">
-            <md-app-toolbar class="md-primary">
-                <span class="md-title">SunScreen</span>
+  <div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">SunScreen</span>
 
-                <div id="nav">
-                    <router-link to="/">Home</router-link> |
-                    <router-link to="/about">About</router-link>
-                </div>
-            </md-app-toolbar>
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      </md-app-toolbar>
 
-            <!-- <md-app-drawer md-permanent="full">
+      <!-- <md-app-drawer md-permanent="full">
                 <md-toolbar class="md-transparent" md-elevation="0">
                     Navigation
                 </md-toolbar>
@@ -38,12 +38,12 @@
                 </md-list>
             </md-app-drawer> -->
 
-            <md-app-content>
-                <!-- Your content here -->
-                <router-view />
-            </md-app-content>
+      <md-app-content>
+        <!-- Your content here -->
+        <router-view />
+      </md-app-content>
 
-            <!-- <div class="phone-viewport">
+      <!-- <div class="phone-viewport">
                 <md-bottom-bar md-sync-route>
                     <md-bottom-bar-item
                         to="/components/bottom-bar"
@@ -63,30 +63,41 @@
                     ></md-bottom-bar-item>
                 </md-bottom-bar>
             </div> -->
-        </md-app>
-    </div>
+    </md-app>
+  </div>
 </template>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
 #nav {
-    padding-left: 30px;
+  padding-left: 30px;
 }
 
 #nav a {
-    color: white;
+  color: white;
 }
 
 #nav a.router-link-exact-active {
-    font-weight: bold;
+  font-weight: bold;
 }
 
-/* .phone-viewport {
-    width: 322px;
-    height: 200px;
-    display: inline-flex;
-    align-items: flex-end;
-    overflow: hidden;
-    border: 1px solid rgba(#000, 0.26);
-    background: rgba(#000, 0.06);
-} */
+/* The app should use the whole html area for the page container, which in turn provides it to the content */
+.page-container {
+  min-height: 100vh;
+}
+
+/** Use the whole available content area (minus the menu bar) for the app content */
+.md-content {
+  min-height: calc(100vh - 64px);
+}
+@media (max-width: 960px) {
+  .md-content {
+    min-height: calc(100vh - 48px);
+  }
+}
+@media (max-width: 600px) {
+  .md-content {
+    min-height: calc(100vh - 56px);
+  }
+}
 </style>
--- src/components/AnyBulb.vue
+++ src/components/AnyBulb.vue
@@ -1,235 +1,209 @@
 <template>
-    <div
-        class="bulb-body"
-        :style="{
-            'background-color': `rgb(${ambientRed * intensity}, ${
-                ambientGreen * intensity
-            }, ${ambientBlue * intensity})`,
-        }"
-    >
-        <!-- Root -->
-        <div class="md-layout md-gutter">
-            <div class="md-layout-item">
-                <md-radio v-model="selectedLightType" value="1"
-                    >High Noon Sun</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="2"
-                    >Direct Sunlight</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="3"
-                    >Overcast Sky</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="4"
-                    >Clear Blue Sky</md-radio
-                >
-                <md-divider></md-divider>
-                <md-radio v-model="selectedLightType" value="5"
-                    >Candle</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="6"
-                    >40W Tungsten</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="7"
-                    >100W Tungsten</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="8"
-                    >Halogen</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="9"
-                    >Carbon Arc</md-radio
-                >
-                <md-divider></md-divider>
-
-                <md-radio v-model="selectedLightType" value="10"
-                    >Warm Fluorescent</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="11"
-                    >Standard Fluorescent</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="12"
-                    >Cool White Fluorescent</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="13"
-                    >Full Spectrum Fluorescent</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="14"
-                    >Grow Light Fluorescent</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="15"
-                    >Black Light Fluorescent</md-radio
-                >
-                <md-divider></md-divider>
-                <md-radio v-model="selectedLightType" value="16"
-                    >Mercury Vapor</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="17"
-                    >Sodium Vapor</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="18"
-                    >Metal Halide</md-radio
-                >
-                <md-radio v-model="selectedLightType" value="19"
-                    >High Pressure Sodium</md-radio
-                >
-                <small>Model value: {{ selectedLightType }}</small>
-            </div>
-        </div>
-        <div class="md-layout md-gutter">
-            <div class="md-layout-item">
-                <md-field>
-                    <label for="movie">Movie</label>
-                    <md-select
-                        v-model="selectedLightType"
-                        name="movie"
-                        id="movie"
-                    >
-                        <md-option value="fight-club">Fight Club</md-option>
-                        <md-option value="godfather">Godfather</md-option>
-                        <md-option value="godfather-ii">Godfather II</md-option>
-                        <md-option value="godfather-iii"
-                            >Godfather III</md-option
-                        >
-                        <md-option value="godfellas">Godfellas</md-option>
-                        <md-option value="pulp-fiction">Pulp Fiction</md-option>
-                        <md-option value="scarface">Scarface</md-option>
-                    </md-select>
-                </md-field>
-            </div>
-            <div class="md-layout-item">
-                <md-field>
-                    <!-- <label for="intensity">Intensity</label> -->
-
-                    <input
-                        name="Intensity"
-                        id="intensity"
-                        class="width-100"
-                        type="range"
-                        v-model.number="intensity"
-                        min="0"
-                        max="1"
-                        step="0.01"
-                    />
-                </md-field>
-            </div>
-        </div>
-    </div>
+  <div
+    class="bulb-body"
+    :style="{
+      'background-color': `rgb(${ambientRed * intensity}, ${
+        ambientGreen * intensity
+      }, ${ambientBlue * intensity})`,
+    }"
+  >
     <!-- Root -->
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item">
+        <md-radio v-model="selectedLightType" value="1">High Noon Sun</md-radio>
+        <md-radio v-model="selectedLightType" value="2"
+          >Direct Sunlight</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="3">Overcast Sky</md-radio>
+        <md-radio v-model="selectedLightType" value="4"
+          >Clear Blue Sky</md-radio
+        >
+        <md-divider></md-divider>
+        <md-radio v-model="selectedLightType" value="5">Candle</md-radio>
+        <md-radio v-model="selectedLightType" value="6">40W Tungsten</md-radio>
+        <md-radio v-model="selectedLightType" value="7">100W Tungsten</md-radio>
+        <md-radio v-model="selectedLightType" value="8">Halogen</md-radio>
+        <md-radio v-model="selectedLightType" value="9">Carbon Arc</md-radio>
+        <md-divider></md-divider>
+
+        <md-radio v-model="selectedLightType" value="10"
+          >Warm Fluorescent</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="11"
+          >Standard Fluorescent</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="12"
+          >Cool White Fluorescent</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="13"
+          >Full Spectrum Fluorescent</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="14"
+          >Grow Light Fluorescent</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="15"
+          >Black Light Fluorescent</md-radio
+        >
+        <md-divider></md-divider>
+        <md-radio v-model="selectedLightType" value="16"
+          >Mercury Vapor</md-radio
+        >
+        <md-radio v-model="selectedLightType" value="17">Sodium Vapor</md-radio>
+        <md-radio v-model="selectedLightType" value="18">Metal Halide</md-radio>
+        <md-radio v-model="selectedLightType" value="19"
+          >High Pressure Sodium</md-radio
+        >
+        <small>Model value: {{ selectedLightType }}</small>
+      </div>
+    </div>
+    <div class="md-layout md-gutter">
+      <!-- <div class="md-layout-item">
+        <md-field>
+          <label for="movie">Movie</label>
+          <md-select v-model="selectedLightType" name="movie" id="movie">
+            <md-option value="fight-club">Fight Club</md-option>
+            <md-option value="godfather">Godfather</md-option>
+            <md-option value="godfather-ii">Godfather II</md-option>
+            <md-option value="godfather-iii">Godfather III</md-option>
+            <md-option value="godfellas">Godfellas</md-option>
+            <md-option value="pulp-fiction">Pulp Fiction</md-option>
+            <md-option value="scarface">Scarface</md-option>
+          </md-select>
+        </md-field>
+      </div> -->
+      <div class="md-layout-item">
+        <md-field>
+          <!-- <label for="intensity">Intensity</label> -->
+
+          <input
+            name="Intensity"
+            id="intensity"
+            class="width-100"
+            type="range"
+            v-model.number="intensity"
+            min="0"
+            max="1"
+            step="0.01"
+          />
+        </md-field>
+      </div>
+    </div>
+  </div>
+  <!-- Root -->
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Options from 'vue-class-component';
-import { Watch, Component, Prop } from 'vue-property-decorator';
-import Color from 'ts-color-class';
+import Vue from "vue";
+import Options from "vue-class-component";
+import { Watch, Component, Prop } from "vue-property-decorator";
+//import Color from "ts-color-class/src/Color.js";
+import Color from "ts-color-class";
 
 @Options({
-    components: {},
+  components: {},
 })
 /** Represents a bulb, emulating the light of a selected source type
  * @devdoc Values taken from https://www.reddit.com/r/spaceengineers/comments/3e0k38/rgb_values_for_various_types_of_realworld_lights/
  */
 export default class AnyBulb extends Vue {
-    // Color values taken from https://www.reddit.com/r/spaceengineers/comments/3e0k38/rgb_values_for_various_types_of_realworld_lights/
-    // highNoonSun
-    // directSunlight
-    // overcastSky
-    // clearBlueSky
-    // candle
-    // fourtyWattTungsten
-    // hundredWattTungsten
-    // halogen
-    // carbonArc
-    // warmFluorescent
-    // standardFluorescent
-    // coolWhiteFluorescent
-    // fullSpectrumFluorescent
-    // growLightFluorescent
-    // blackLightFluorescent
-    // mercuryVapor
-    // sodiumVapor
-    // metalHalide
-    // highPressureSodium
+  // Color values taken from https://www.reddit.com/r/spaceengineers/comments/3e0k38/rgb_values_for_various_types_of_realworld_lights/
+  // highNoonSun
+  // directSunlight
+  // overcastSky
+  // clearBlueSky
+  // candle
+  // fourtyWattTungsten
+  // hundredWattTungsten
+  // halogen
+  // carbonArc
+  // warmFluorescent
+  // standardFluorescent
+  // coolWhiteFluorescent
+  // fullSpectrumFluorescent
+  // growLightFluorescent
+  // blackLightFluorescent
+  // mercuryVapor
+  // sodiumVapor
+  // metalHalide
+  // highPressureSodium
 
-    //TODO use the color type everywhere
-    highNoonSunColor = new Color([255, 255, 251]);
+  highNoonSunColor = new Color.Color([255, 255, 251]);
+  directSunlightColor = new Color.Color([255, 255, 255]);
+  overcastSkyColor = new Color.Color([201, 226, 255]);
+  clearBlueSkyColor = new Color.Color([64, 156, 255]);
+  candleColor = new Color.Color([255, 147, 41]);
+  fourtyWattTungstenColor = new Color.Color([255, 197, 143]);
+  hundredWattTungstenColor = new Color.Color([255, 214, 170]);
+  halogenColor = new Color.Color([255, 241, 224]);
+  carbonArcColor = new Color.Color([255, 250, 244]);
+  warmFluorescentColor = new Color.Color([255, 244, 229]);
+  standardFluorescentColor = new Color.Color([244, 255, 250]);
+  coolWhiteFluorescentColor = new Color.Color([212, 235, 255]);
+  fullSpectrumFluorescentColor = new Color.Color([255, 244, 242]);
+  growLightFluorescentColor = new Color.Color([255, 239, 247]);
+  blackLightFluorescentColor = new Color.Color([167, 0, 255]);
+  mercuryVaporColor = new Color.Color([216, 247, 255]);
+  sodiumVaporColor = new Color.Color([255, 209, 178]);
+  metalHalideColor = new Color.Color([242, 252, 255]);
+  highPressureSodiumColor = new Color.Color([255, 183, 76]);
 
-    highNoonSun = { red: 255, green: 255, blue: 251 };
-    directSunlight = { red: 255, green: 255, blue: 255 };
-    overcastSky = { red: 201, green: 226, blue: 255 };
-    clearBlueSky = { red: 64, green: 156, blue: 255 };
-    candle = { red: 255, green: 147, blue: 41 };
-    fourtyWattTungsten = { red: 255, green: 197, blue: 143 };
-    hundredWattTungsten = { red: 255, green: 214, blue: 170 };
-    halogen = { red: 255, green: 241, blue: 224 };
-    carbonArc = { red: 255, green: 250, blue: 244 };
-    warmFluorescent = { red: 255, green: 244, blue: 229 };
-    standardFluorescent = { red: 244, green: 255, blue: 250 };
-    coolWhiteFluorescent = { red: 212, green: 235, blue: 255 };
-    fullSpectrumFluorescent = { red: 255, green: 244, blue: 242 };
-    growLightFluorescent = { red: 255, green: 239, blue: 247 };
-    blackLightFluorescent = { red: 167, green: 0, blue: 255 };
-    mercuryVapor = { red: 216, green: 247, blue: 255 };
-    sodiumVapor = { red: 255, green: 209, blue: 178 };
-    metalHalide = { red: 242, green: 252, blue: 255 };
-    highPressureSodium = { red: 255, green: 183, blue: 76 };
+  lightTypes = [
+    { id: "1", color: this.highNoonSunColor },
+    { id: "2", color: this.directSunlightColor },
+    { id: "3", color: this.overcastSkyColor },
+    { id: "4", color: this.clearBlueSkyColor },
+    { id: "5", color: this.candleColor },
+    { id: "6", color: this.fourtyWattTungstenColor },
+    { id: "7", color: this.hundredWattTungstenColor },
+    { id: "8", color: this.halogenColor },
+    { id: "9", color: this.carbonArcColor },
+    { id: "10", color: this.warmFluorescentColor },
+    { id: "11", color: this.standardFluorescentColor },
+    { id: "12", color: this.coolWhiteFluorescentColor },
+    { id: "13", color: this.fullSpectrumFluorescentColor },
+    { id: "14", color: this.growLightFluorescentColor },
+    { id: "15", color: this.blackLightFluorescentColor },
+    { id: "16", color: this.mercuryVaporColor },
+    { id: "17", color: this.sodiumVaporColor },
+    { id: "18", color: this.metalHalideColor },
+    { id: "19", color: this.highPressureSodiumColor },
+  ];
 
-    lightTypes = [
-        { id: '1', color: this.highNoonSun },
-        { id: '2', color: this.directSunlight },
-        { id: '3', color: this.overcastSky },
-        { id: '4', color: this.clearBlueSky },
-        { id: '5', color: this.candle },
-        { id: '6', color: this.fourtyWattTungsten },
-        { id: '7', color: this.hundredWattTungsten },
-        { id: '8', color: this.halogen },
-        { id: '9', color: this.carbonArc },
-        { id: '10', color: this.warmFluorescent },
-        { id: '11', color: this.standardFluorescent },
-        { id: '12', color: this.coolWhiteFluorescent },
-        { id: '13', color: this.fullSpectrumFluorescent },
-        { id: '14', color: this.growLightFluorescent },
-        { id: '15', color: this.blackLightFluorescent },
-        { id: '16', color: this.mercuryVapor },
-        { id: '17', color: this.sodiumVapor },
-        { id: '18', color: this.metalHalide },
-        { id: '19', color: this.highPressureSodium },
-    ];
+  selectedLightType = "1";
+  ambientRed = 255;
+  ambientGreen = 255;
+  ambientBlue = 255;
+  intensity = 1;
 
-    selectedLightType = '1';
-    ambientRed = 255;
-    ambientGreen = 255;
-    ambientBlue = 255;
-    intensity = 1;
+  mounted() {
+    //Preselect an option
+    this.selectedLightType = this.lightTypes[6].id;
+    this.lightTypeChanged(this.selectedLightType);
+  }
 
-    mounted() {
-        //Preselect an option
-        this.selectedLightType = this.lightTypes[6].id;
-        this.lightTypeChanged(this.selectedLightType);
+  @Watch("selectedLightType")
+  lightTypeChanged(newValue: string) {
+    for (var lightType of this.lightTypes) {
+      if (lightType.id === newValue) {
+        this.ambientRed = lightType.color.getRed();
+        this.ambientGreen = lightType.color.getGreen();
+        this.ambientBlue = lightType.color.getBlue();
+      }
     }
-
-    @Watch('selectedLightType')
-    lightTypeChanged(newValue: string) {
-        for (var lightType of this.lightTypes) {
-            if (lightType.id === newValue) {
-                this.ambientRed = lightType.color.red;
-                this.ambientGreen = lightType.color.green;
-                this.ambientBlue = lightType.color.blue;
-            }
-        }
-    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .md-radio {
-    /* display: flex; */
-    background-color: darkslategray;
-    background-color: rgba(255, 255, 255, 0.2);
+  /* display: flex; */
+  /* background-color: darkslategray; */
+  background-color: rgba(255, 255, 255, 0.2);
 }
 
 /** A control, representing the daytime clock */
 .width-100 {
-    width: 100%;
+  width: 100%;
 }
 </style>
