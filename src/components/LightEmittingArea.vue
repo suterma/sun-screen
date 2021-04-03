eb811f4a1712fb52073072bf08714402d86b28ac
suterma <git@marcelsuter.ch>
2021-04-03 14:15:32 +0200

    added gradient slider

--- src/components/LightEmittingArea.vue
+++ src/components/LightEmittingArea.vue
@@ -1,17 +1,15 @@
 <template>
-    <div>
-        <!-- background: rgb(2,0,36);
-background: linear-gradient(110deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 38%, rgba(0,212,255,1) 46%); -->
-
-        <!-- 'background-color': `rgb(${color.getRed()}, ${color.getGreen()}, ${color.getBlue()})`, -->
-
-        <div
-            class="light-emitting-area"
-            :style="{
-                background: `linear-gradient(90deg, rgb(${color.getRed()}, ${color.getGreen()}, ${color.getBlue()}) 40%, rgb(${colorSecondary.getRed()}, ${colorSecondary.getGreen()}, ${colorSecondary.getBlue()}) 60%)`,
-            }"
-        ></div>
-    </div>
+    <div
+        class="light-emitting-area"
+        :style="{
+            background: `linear-gradient(90deg, 
+            rgb(${color.getRed()}, ${color.getGreen()}, ${color.getBlue()}) 
+            ${lowerSplitPosition}%, 
+            rgb(${colorSecondary.getRed()}, ${colorSecondary.getGreen()}, ${colorSecondary.getBlue()}) 
+            ${upperSplitPosition}%
+            )`,
+        }"
+    ></div>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
@@ -26,6 +24,19 @@ export default class LightEmittingArea extends Vue {
 
     @Prop({ required: true, type: Color, default: new Color('white') })
     colorSecondary!: Color;
+
+    @Prop({ required: false, type: Number, default: 50 })
+    splitPosition!: number;
+
+    /** The width of the split area */
+    splitWidth = 2;
+
+    get lowerSplitPosition(): number {
+        return this.splitPosition - 2;
+    }
+    get upperSplitPosition(): number {
+        return this.splitPosition + 2;
+    }
 }
 </script>
 
